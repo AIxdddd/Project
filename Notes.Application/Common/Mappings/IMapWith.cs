58bdabb8015c0cecb06978d0a4faf1fe3b777303
Ilya <i.e.agapov.king@mail.ru>
2024-07-10 02:46:07 +0300

    add Mapper

---
+++ Notes.Application/Common/Mappings/IMapWith.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using AutoMapper;
+using Project.Application.Common.Mappings;
+namespace Project.Application.Common.Mappings
+{
+    public interface IMapWith<T>
+    {
+        void Mapping(Profile profile) =>
+            profile.CreateMap(typeof(T), GetType());
+    }
+}
